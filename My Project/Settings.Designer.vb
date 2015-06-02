9b1acfb867873c07a3f864009c3a10964e278a14
gmontalvo5416 <gabe.mon5@outlook.com>
2015-06-02 15:54:36 -0700

    update

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
 
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
-        Friend ReadOnly Property Settings() As Global.MPGP.My.MySettings
+        Friend ReadOnly Property Settings() As Global.CryptVB.My.MySettings
             Get
-                Return Global.MPGP.My.MySettings.Default
+                Return Global.CryptVB.My.MySettings.Default
             End Get
         End Property
     End Module
