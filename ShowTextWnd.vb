b9b97d3fccebae6802438ee5c4dda0d699200b12
Gabriel Montalvo <gmontalvo5416@users.noreply.github.com>
2015-07-11 19:11:28 -0700

    Update ShowTextWnd.vb

--- ShowTextWnd.vb
+++ ShowTextWnd.vb
@@ -3,11 +3,8 @@
        Optional ByVal Title As Object = Nothing _
     ) As DialogResult
 
-        'Set other properties
         txtMsg.Text = Text
         Me.Text = Title
-        'Button1.DialogResult = DialogResult.OK
-
 
         'Display the form modally and return its DialogResult
         Return ShowDialog()
